3e5af1f0cf1fafd60e21d6fb53ee445a9555c414
SliceOfKekus <sliceofkekus@icloud.com>
2021-02-22 22:08:49 +0300

    Done with change color.
    Is it cool? idk :think:

---
+++ lesson 1.5 - Shaders/Exercises/Move_Triangle_Via_Uniform/ShaderProgram.cpp
@@ -0,0 +1,91 @@
+#include "ShaderProgram.h"
+
+void ShaderProgram::CompileShader(GLuint index, const GLchar* source)
+{
+	GLchar log[512];
+	GLint success;
+
+	glShaderSource(index, 1, &source, nullptr);
+	glCompileShader(index);
+	glGetProgramiv(index, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetProgramInfoLog(index, sizeof(log), nullptr, log);
+		std::cout << "ERROR::SHADER::COMPILATION_FAILED\n";
+		std::cout << log << "\n";
+		return;
+	}
+
+	glAttachShader(this->program, index);
+}
+
+void ShaderProgram::Link()
+{
+	GLchar log[512];
+	GLint success;
+
+	glLinkProgram(this->program);
+	glGetProgramiv(this->program, GL_LINK_STATUS, &success);
+
+	if (!success)
+	{
+		glGetProgramInfoLog(this->program, sizeof(log), nullptr, log);
+		std::cout << "ERROR::PROGRAM::LINKING_FAILED\n";
+		std::cout << log << "\n";
+	}
+}
+
+ShaderProgram::ShaderProgram(const char* vertexPath, const char* fragmentPath)
+{
+	std::string vertexCode;
+	std::string fragmentCode;
+	std::ifstream vertexFile;
+	std::ifstream fragmentFile;
+
+	vertexFile.exceptions(std::ifstream::failbit);
+	fragmentFile.exceptions(std::ifstream::failbit);
+
+	try
+	{
+		std::stringstream vShader;
+		std::stringstream fShader;
+
+		vertexFile.open(vertexPath);
+		fragmentFile.open(fragmentPath);
+
+		vShader << vertexFile.rdbuf();
+		fShader << fragmentFile.rdbuf();
+		
+		vertexCode = vShader.str();
+		fragmentCode = fShader.str();
+	}
+	catch (std::ifstream::failure e)
+	{
+		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ\n";
+		std::cout << e.what() << "\n";
+	}
+
+	const GLchar* vShader = vertexCode.c_str();
+	const GLchar* fShader = fragmentCode.c_str();
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	
+	this->program = glCreateProgram();
+	CompileShader(vertexShader, vShader);
+	CompileShader(fragmentShader, fShader);
+	Link();
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+}
+
+GLint ShaderProgram::GetIndex()
+{
+	return this->program;
+}
+
+void ShaderProgram::Use()
+{
+	glUseProgram(this->program);
+}
---
+++ lesson 1.5 - Shaders/Exercises/Move_Triangle_Via_Uniform/ShaderProgram.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <glew.h>
+
+class ShaderProgram
+{
+	GLint program;
+	void CompileShader(GLuint, const GLchar*);
+	void Link();
+public:
+	ShaderProgram(const char*, const char*);
+	GLint GetIndex();
+	void Use();
+};
+
